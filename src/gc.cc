32c1acccb153257d5ed90dbdb6985b43f515d283
李韻晟 <liyuncheng@MacBook-Pro.local>
2020-07-07 23:59:08 +0800

    define struct

--- src/gc.cc
+++ src/gc.cc
@@ -1,2 +1,29 @@
 #include <nan.h>
 #include <math.h>
+
+using namespace v8;
+
+struct HeapInfo
+{
+  /* data */
+  size_t totalHeapSize;
+  size_t totalHeapExecutableSize;
+  size_t totalPhysicalSize;
+  size_t usedHeapSize;
+  size_t heapSizeLimit;
+  size_t totalAvailableSize;
+  size_t mallocedMemory;
+  size_t peakMallocedMemory;
+  size_t numberOfNativeContexts;
+  size_t numberOfDetachedContexts;
+};
+
+struct HeapData
+{
+  /* data */
+  HeapInfo *before;
+  HeapInfo *after;
+  uint64_t gcStartTime;
+  uint64_t gcEndTime;
+  int gcType;
+};
